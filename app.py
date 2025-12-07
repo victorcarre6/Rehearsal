ca598177d940ec38c1f2c65d07e983b1af3ecb53
victor carre <victorcarre6@gmail.com>
2025-12-07 19:31:51 +0100

    streamlit interface

--- app.py
+++ app.py
@@ -49,11 +49,6 @@ st.markdown("""
         color: #000000 !important;
     }
 
-    /* Forcer aussi le label du multiselect */
-    div[data-baseweb="select"] * {
-        color: #000000 !important;
-    }
-
     /* Couleur de la zone d’écriture des widgets */
     .stMultiSelect, .stSelectbox, .stNumberInput input {
         color: #000000 !important;
@@ -247,7 +242,7 @@ filtered_themes = filter_questions_by_tags(
 )
 
 # Section de sélection du thème
-st.markdown("###Sélectionner un thème")
+st.markdown("Sélectionner un thème")
 
 # Créer des colonnes pour les boutons de thème
 cols = st.columns(3)
